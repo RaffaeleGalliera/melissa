2810f663d2208466173b3a1ce8ebd49c3059bf04
Raffaele Galliera <gallieraraffaele@gmail.com>
2022-11-17 13:07:40 -0600

    chore: add multi discrete to discrete and action wrappers

---
+++ simple_mpr/env/utils/wrappers/action_wrapper.py
@@ -0,0 +1,18 @@
+"""
+Wrapper to modify actions before step is taken
+"""
+
+from pettingzoo.utils.wrappers.base import BaseWrapper
+
+
+class ActionWrapper(BaseWrapper):
+    def step(self, action):
+        return super().step(self.action(action))
+
+    def action(self, action):
+        """Returns a modified action before `env.step` is called."""
+        raise NotImplementedError
+
+    def reverse_action(self, action):
+        """Returns a reversed ``action``."""
+        raise NotImplementedError
