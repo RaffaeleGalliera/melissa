1b04bfa0326222d41a8548467cd94f5d8fe35b84
Raffaele Galliera <gallieraraffaele@gmail.com>
2023-01-16 16:14:02 -0600

    chore: add networks using GNNs in a separate file

---
+++ graph_env/network_policies/networks.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch_geometric.nn import GATConv
+
+
+class GATNetwork(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, num_heads, device='cpu'):
+        super(GATNetwork, self).__init__()
+        self.device = device
+        self.conv1 = GATConv(input_dim, hidden_dim, num_heads)
+        self.fc1 = nn.Linear(hidden_dim * num_heads, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, obs, state=None, info={}):
+        logits = []
+        for observation in obs.observation:
+            # TODO: Need here we move tensors to CUDA, cannot just put it in Batch because of data time -> slows down
+            x = torch.as_tensor(observation[2], device=self.device, dtype=torch.float32)
+            edge_index = torch.as_tensor(observation[0], device=self.device, dtype=torch.int)
+            x = self.conv1(x, edge_index)
+            x = x.view(x.size(0), -1)
+            x = F.relu(self.fc1(x))
+            x = self.fc2(x)
+            logits.append(x[observation[3]].flatten())
+        logits = torch.stack(logits)
+        return logits, state
+
