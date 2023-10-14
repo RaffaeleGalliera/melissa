2ecc45343b81f515e0651ce92eaff4c3d5d9ea35
Raffaele Galliera <gallieraraffaele@gmail.com>
2023-10-14 17:51:58 -0500

    chore(reward): fix shared transmission guard

--- graph_env/env/graph.py
+++ graph_env/env/graph.py
@@ -343,7 +343,7 @@ class GraphEnv(AECEnv):
 
         reward = agent.two_hop_cover / len(two_hop_neighbor_indices)
         if agent.action:
-            penalty_1 = sum([self.world.agents[index].action for index in one_hop_neighbor_indices]) / len(one_hop_neighbor_indices)
+            penalty_1 = sum([1 for index in one_hop_neighbor_indices if self.world.agents[index].action]) / len(one_hop_neighbor_indices)
             reward = reward - penalty_1
         if not sum(agent.state.transmitted_to):
             uncovered_n_lens = [len(np.where(self.world.agents[index].one_hop_neighbours_ids)[0]) for index in one_hop_neighbor_indices if
